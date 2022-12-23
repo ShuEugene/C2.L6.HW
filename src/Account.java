f2933782d7319058bd67424136705cea01d7576e
Евгений А. Шуликов <shueugene@hotmail.com>
2022-12-23 22:05:56 +0300

    C2.L6.HW1.ver.02 is complete.

--- src/Account.java
+++ src/Account.java
@@ -1,31 +1,35 @@
 public class Account {
 
-    public static boolean isChecked(String string) {
-        return string != null && !string.isEmpty() && !string.isBlank() && string.length() <= 20
-                && string.matches("\\w+");
+    public static boolean checkFailed(String string) {
+        return string == null || string.isEmpty() || string.isBlank() || string.length() > 20
+                || !string.matches("\\w+");
     }
 
     private final String login;
     private String password, confirmPassword;
 
 
-    public Account(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        if (isChecked(login)) {
-            this.login = login;
-        } else {
-            throw new WrongLoginException("Логин содержит запрещённые символы.");
-        }
-        if (isChecked(password)) {
-            if (confirmPassword.equals(password)) {
-                this.password = password;
-                this.confirmPassword = confirmPassword;
-            } else {
-                throw new WrongPasswordException("Пароль не подтверждён.");
-            }
-        } else {
-            throw new WrongPasswordException("Пароль содержит запрещённые символы.");
-        }
+    public Account(String login, String password, String confirmPassword) {
+        this.login = login;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
     }
+
+
+    public boolean isValid() throws WrongLoginException, WrongPasswordException {
+        if (checkFailed(login)) {
+            throw new WrongLoginException("Логин аккаунта «" + getLogin() + "» содержит запрещённые символы.");
+        }
+        if (checkFailed(password)) {
+            throw new WrongPasswordException("Пароль аккаунта «" + getLogin() + "» содержит запрещённые символы.");
+        } else {
+            if (!confirmPassword.equals(password)) {
+                throw new WrongPasswordException("Пароль аккаунта «" + getLogin() + "» не подтверждён.");
+            }
+        }
+        return true;
+    }
+
 
     public final String getLogin() {
         return login;
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        Account sarahConor = new Account("Sarah_Conor", "жara1conor", "sara");
         try {
-            Account account = new Account("Sara_Conor", "жara1conor", "sara");
+            if (sarahConor.isValid()) {
+                System.out.println("\nЛогин и пароль аккаунта " + sarahConor.getLogin() + " прошли проверку.");
+            }
         } catch (WrongLoginException | WrongPasswordException e) {
             System.out.println("\n" + e.getMessage());
         }
 
+        Account duncanMcLoud = new Account("Duncan_McL0ud", "pas123word", "pas123word");
+        try {
+            if (duncanMcLoud.isValid()) {
+                System.out.println("\nЛогин и пароль аккаунта " + duncanMcLoud.getLogin() + " прошли проверку.");
+            }
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("\n" + e.getMessage());
+        }
     }
 }
